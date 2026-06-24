e7cd8ef493c04ed78a6f583f030c9e6406288505
adithyank1998 <8adi1998@gmail.com>
2026-06-24 22:49:14 +0530

    urls.py updated

--- appointments/urls.py
+++ appointments/urls.py
@@ -8,6 +8,13 @@ urlpatterns = [
     path('doctors/<int:pk>/delete/', views.doctor_delete, name='doctor_delete'),
     path('', views.appointment_list, name='appointment_list'),
     path('book/', views.appointment_create, name='appointment_create'),
+    path('<int:pk>/', views.appointment_detail, name='appointment_detail'),
     path('<int:pk>/edit/', views.appointment_update, name='appointment_update'),
     path('<int:pk>/delete/', views.appointment_delete, name='appointment_delete'),
+    path('<int:appointment_pk>/prescription/add/', views.prescription_create, name='prescription_create'),
+    path('prescription/<int:pk>/edit/', views.prescription_edit, name='prescription_edit'),
+    path('my-prescriptions/', views.my_prescriptions, name='my_prescriptions'),
+    path('profile/', views.patient_profile, name='patient_profile'),
+    path('patient/<int:pk>/', views.patient_detail, name='patient_detail'),
+    path('doctor-dashboard/', views.doctor_dashboard, name='doctor_dashboard'),
 ]
