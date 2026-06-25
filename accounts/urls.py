6d75909deed639da0645d5aaa78d1f89ace46864
adithyank1998 <8adi1998@gmail.com>
2026-06-26 01:18:06 +0530

    user details updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('login/', views.ClinicLoginView.as_view(), name='login'),
     path('logout/', views.ClinicLogoutView.as_view(), name='logout'),
     path('users/', views.user_list, name='user_list'),
+    path('users/create/', views.user_create, name='user_create'),
+    path('users/<int:pk>/', views.user_detail, name='user_detail'),
     path('users/<int:pk>/edit/', views.user_update, name='user_update'),
 ]
